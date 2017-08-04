4e9224c56bd18c059e376725984e47d25bef9e60
Erdem Şahin <erdem.sahin.uslu@gmail.com>
2017-08-05 02:54:54 +0300

    Added Product.

---
+++ db/migrate/20170804235439_create_products.rb
@@ -0,0 +1,14 @@
+class CreateProducts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.string :slug
+      t.text :description
+      t.decimal :price
+      t.references :brand, foreign_key: true
+      t.references :category, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/products.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :product do
+    name "MyString"
+    slug "MyString"
+    description "MyText"
+    price "9.99"
+    brand nil
+    category nil
+  end
+end
